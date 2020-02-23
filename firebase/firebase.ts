d21d76a5b0602c8c84dfe6f7ae31375f2218c43d
TSmith <tom.smith2711@gmail.com>
2020-02-23 20:39:30 +0000

    use correct api keys

--- firebase/firebase.ts
+++ firebase/firebase.ts
@@ -1,18 +1,18 @@
 import firebase from 'firebase';
 let firebaseConfig = {};
 if (!firebase.apps.length) {
-	//if (process.env.NODE_ENV == 'development') {
-	firebaseConfig = {
-		apiKey: 'AIzaSyBL86-Tdgy08hGdTauUbF53_1qVPvIGrcE',
-		authDomain: 'recycle-dev-7cf5f.firebaseapp.com',
-		databaseURL: 'https://recycle-dev-7cf5f.firebaseio.com',
-		projectId: 'recycle-dev-7cf5f',
-		storageBucket: 'recycle-dev-7cf5f.appspot.com',
-		messagingSenderId: '517490912042',
-		appId: '1:517490912042:web:c113d543df4425d629cc53',
-		measurementId: 'G-TBRGN9VP4J'
-	};
-	/* 	} else {
+	if (process.env.NODE_ENV == 'development') {
+		firebaseConfig = {
+			apiKey: 'AIzaSyBL86-Tdgy08hGdTauUbF53_1qVPvIGrcE',
+			authDomain: 'recycle-dev-7cf5f.firebaseapp.com',
+			databaseURL: 'https://recycle-dev-7cf5f.firebaseio.com',
+			projectId: 'recycle-dev-7cf5f',
+			storageBucket: 'recycle-dev-7cf5f.appspot.com',
+			messagingSenderId: '517490912042',
+			appId: '1:517490912042:web:c113d543df4425d629cc53',
+			measurementId: 'G-TBRGN9VP4J'
+		};
+	} else {
 		firebaseConfig = {
 			apiKey: 'AIzaSyDtlqeARjKr9xAENfapfqSb3s7iDq2WYMQ',
 			authDomain: 'recycler-99415.firebaseapp.com',
@@ -23,7 +23,7 @@ if (!firebase.apps.length) {
 			appId: '1:475097617401:web:39d109085b3c8c5291a328',
 			measurementId: 'G-FYRZ1GWRX2'
 		};
-	} */
+	}
 	// Your web app's Firebase configuration
 
 	// Initialize Firebase
