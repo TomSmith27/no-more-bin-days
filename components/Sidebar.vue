154b1d075047d8f0bad687221cc04def76544b9b
TSmith <tom.smith2711@gmail.com>
2020-02-25 16:02:01 +0000

    Finish sidebar

--- components/Sidebar.vue
+++ components/Sidebar.vue
@@ -1,31 +1,8 @@
 <template>
-  <div class="sidebar-container">
+  <div class="sidebar-container text-right">
     <div v-if="value" class="modal-backdrop" @click="$emit('input', false)"></div>
-    <div class="sidebar" :class="{'sidebar-show' : value}">
-      <b-navbar-nav class="text-right">
-        <b-nav-item-dropdown text="Products" right>
-          <b-dropdown-item :to="{name : 'Products'}">A-Z</b-dropdown-item>
-          <b-dropdown-item :to="{name : 'Locations'}">Categories</b-dropdown-item>
-        </b-nav-item-dropdown>
-        <b-nav-item-dropdown text="Locations" right>
-          <b-dropdown-item class="d-none d-xl-block" :to="{name : 'shops'}">A-Z</b-dropdown-item>
-          <b-dropdown-item>Categories</b-dropdown-item>
-        </b-nav-item-dropdown>
-        <b-nav-item :to="{name : 'Bins' }">Bins</b-nav-item>
-        <b-nav-item :to="{name : 'About' }">About</b-nav-item>
-        <b-nav-item :to="{name : 'News' }">News</b-nav-item>
-        <b-nav-item :to="{name : 'DidYouKnow' }">Did you know?</b-nav-item>
-
-        <!--    <b-nav-item href="#">Blogs</b-nav-item>
-          <b-nav-item href="#">Bins</b-nav-item>
-        <b-nav-item href="#">Composting</b-nav-item>-->
-        <client-only placeholder="Loading...">
-          <!--        <b-nav-item-dropdown v-if="isLoggedIn" text="Admin" right>
-            <b-dropdown-item :to="{name : 'ProductAdmin'}">Product Admin</b-dropdown-item>
-            <b-dropdown-item :to="{name : 'ShopAdmin'}">Shop Admin</b-dropdown-item>
-          </b-nav-item-dropdown>-->
-        </client-only>
-      </b-navbar-nav>
+    <div class="sidebar bg-white shadow p-3" :class="{'sidebar-show' : value}">
+      <slot />
     </div>
   </div>
 </template>
@@ -52,7 +29,6 @@ export default {
   top: 0;
   right: -30vw;
   width: 30vw;
-  background: red;
   transition: right 0.5s;
 
   &-show {
--- layouts/default.vue
+++ layouts/default.vue
@@ -3,8 +3,66 @@
     <b-navbar toggleable="lg" type="light" sticky class="bg-white shadow-sm">
       <b-navbar-brand tag="h1" class="mb-0" :to="{name : 'index'}">No More Bin Days</b-navbar-brand>
 
-      <b-navbar-toggle @click="navOpen = true"></b-navbar-toggle>
-      <sidebar v-model="navOpen" />
+      <b-navbar-toggle target="null" @click="navOpen = true"></b-navbar-toggle>
+      <sidebar v-model="navOpen">
+        <b-navbar-nav>
+          <b-nav-item-dropdown text="Products" right>
+            <b-dropdown-item :to="{name : 'Products'}">A-Z</b-dropdown-item>
+            <b-dropdown-item :to="{name : 'Locations'}">Categories</b-dropdown-item>
+          </b-nav-item-dropdown>
+          <b-nav-item-dropdown text="Locations" right>
+            <b-dropdown-item class="d-none d-xl-block" :to="{name : 'shops'}">A-Z</b-dropdown-item>
+            <b-dropdown-item>Categories</b-dropdown-item>
+          </b-nav-item-dropdown>
+          <b-nav-item :to="{name : 'Bins' }">Bins</b-nav-item>
+          <b-nav-item :to="{name : 'About' }">About</b-nav-item>
+          <b-nav-item :to="{name : 'News' }">News</b-nav-item>
+          <b-nav-item :to="{name : 'DidYouKnow' }">Did you know?</b-nav-item>
+
+          <!--    <b-nav-item href="#">Blogs</b-nav-item>
+          <b-nav-item href="#">Bins</b-nav-item>
+          <b-nav-item href="#">Composting</b-nav-item>-->
+          <client-only placeholder="Loading...">
+            <b-nav-item-dropdown v-if="isLoggedIn" text="Admin" right>
+              <b-dropdown-item :to="{name : 'ProductAdmin'}">Product Admin</b-dropdown-item>
+              <b-dropdown-item :to="{name : 'ShopAdmin'}">Shop Admin</b-dropdown-item>
+            </b-nav-item-dropdown>
+          </client-only>
+        </b-navbar-nav>
+        <b-navbar-nav>
+          <client-only placeholder="Loading...">
+            <b-nav-item v-if="!isLoggedIn">
+              <b-button v-b-modal.login-modal variant="outline-primary">Login</b-button>
+              <b-modal hide-footer title="Login" centered id="login-modal">
+                <b-form @submit.prevent="onSubmit">
+                  <b-form-group id="input-group-1" label="Email address:" label-for="input-1">
+                    <b-form-input id="input-1" v-model="form.email" type="email" required></b-form-input>
+                  </b-form-group>
+
+                  <b-form-group id="input-group-2" label="Password" label-for="input-2">
+                    <b-form-input id="input-2" v-model="form.password" type="password" required></b-form-input>
+                  </b-form-group>
+
+                  <div class="d-flex justify-content-between w-100">
+                    <button class="btn btn-secondary" type="button" @click="$bvModal.hide('login-modal')">Cancel</button>
+                    <button class="btn btn-primary" type="submit">Login</button>
+                  </div>
+                </b-form>
+              </b-modal>
+            </b-nav-item>
+            <b-nav-item-dropdown v-else right>
+              <!-- Using 'button-content' slot -->
+              <template v-slot:button-content>
+                <em>{{user.email}}</em>
+              </template>
+              <b-dropdown-item @click="logout">Log out</b-dropdown-item>
+            </b-nav-item-dropdown>
+          </client-only>
+          <b-nav-item class="position-absolute" style="bottom : 0; right : 5">
+            <small>{{ version }}</small>
+          </b-nav-item>
+        </b-navbar-nav>
+      </sidebar>
 
       <b-navbar-nav class="text-right d-none d-lg-flex">
         <b-nav-item-dropdown text="Products" right>
@@ -95,7 +153,7 @@ export default Vue.extend({
     }
   },
   watch: {
-    $route: function() {
+    $route: function () {
       this.navOpen = false
     }
   },
